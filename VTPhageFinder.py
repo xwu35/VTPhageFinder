54e18b67a6142bf96e4dd88da21fcb12fd8773fe
Xiaofen Wu <xiaofen1224@gmail.com>
2026-02-24 09:34:05 -0700

    bump to v1.1.0

--- VTPhageFinder.py
+++ VTPhageFinder.py
@@ -6,8 +6,16 @@ import subprocess
 import click
 from datetime import datetime
 
-version = "1.0.0"
+version = "1.1.0"
 @click.version_option(version, "--version", "-v")
+
+def validate_test_run(ctx, param, value):
+    """
+    A callback to make --reads_dir and --sample_info required if --test is not specified.
+    """
+    if not ctx.params.get('test') and value is None:
+        raise click.BadParameter(f"Option '{param.name}' is required unless --test is used.")
+    return value
 
 @click.command(
     context_settings=dict(help_option_names=['-h', '--help'], max_content_width=150),
@@ -16,13 +24,13 @@ version = "1.0.0"
 )
 @click.option(
     '--reads_dir',
-    required=True,
+    callback=validate_test_run,
     type=click.Path(dir_okay=True, exists=True, resolve_path=True),
     help='Reads directory'
 )
 @click.option(
     '--sample_info',
-    required=True,
+    callback=validate_test_run,
     type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True, resolve_path=True),
     help='Sample information table (tab separated).' 
     ' The table must contain three columns (sample, R1, R2)'
@@ -36,13 +44,13 @@ version = "1.0.0"
 )
 @click.option(
     '--reference_genome',
-    required=True,
+    callback=validate_test_run,
     type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True, resolve_path=True),
     help=('Reference genome sequences')
 )
 @click.option(
     '--prophage_region',
-    required=True,
+    callback=validate_test_run,
     type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True, resolve_path=True),
     help=('Prophage coordinates on the host genome')
 )
@@ -59,6 +67,13 @@ version = "1.0.0"
     type=int,
     show_default=True,
     help=('Percent of identity to remove reads mapped to the non-prophage region')
+)
+@click.option(
+    '--adapter',
+    default='',
+    type=str,
+    show_default=True,
+    help=('Adapter sequences. By default, the adapters.fna file within `VTPhageFinder/db` is used')
 )
 @click.option(
     '--mapper',
@@ -82,6 +97,13 @@ version = "1.0.0"
     help=('Steps to run; available options are: fastqc, preprocess, assemble')
 )
 @click.option(
+    '--test',
+    is_flag=True,
+    default=False,
+    show_default=True,
+    help='test run'
+)
+@click.option(
     '--dryrun',
     is_flag=True,
     default=False,
@@ -93,7 +115,7 @@ version = "1.0.0"
     default='',
     show_default=True,
     help='Directory to store conda environments.'
-    ' By default, the ".snakemake" directory relative to the invocation directory is used'
+    ' By default, the "conda_env" directory within VTPhageFinder is used'
 )
 @click.option(
     '--profile',
@@ -104,40 +126,54 @@ version = "1.0.0"
 
 def run_vtphagefinder(reads_dir, sample_info, output_dir, reference_genome, 
           prophage_region, prophage_identity, nonprophage_identity,
-          mapper, assembler, step, dryrun, conda_envs, profile):
+          adapter, mapper, assembler, step, test, dryrun, conda_envs, profile):
+            
+    # get snakefile, default adapter and default conda envs path
+    script_dir=os.path.dirname(os.path.abspath(__file__))
+    snakefile=os.path.join(script_dir, "workflow", "Snakefile")
+    default_adapters=os.path.join(script_dir, "db", "adapters.fna")
+    default_envs=os.path.join(script_dir, "conda_envs")
 
-          # write run log if it is not a dry run
-          if not dryrun:
-            os.makedirs(output_dir, exist_ok=True)
-            logfile = os.path.join(output_dir, f"{os.path.basename(output_dir)}_run.log")
-            with open(logfile, "w") as log:
-                log.write("================VTPhageFinder run log==============\n")
-                log.write(f"Start time: {datetime.now()}\n")
-                log.write(f"VTPhageFinder version: {version}\n")
-                log.write(f"Raw reads direcotry: {reads_dir}\n")
-                log.write(f"Sample table: {sample_info}\n")
-                log.write(f"Results directory: {output_dir}\n")
-                log.write(f"Host genome: {reference_genome}\n")
-                log.write(f"Host prophage coordinates: {prophage_region}\n")
-                log.write(f"Host prophage region identity cutoff: {prophage_identity}\n")
-                log.write(f"Host non-prophage region identity cutoff: {nonprophage_identity}\n")
-                log.write(f"Mapping software: {mapper}\n")
-                log.write(f"Assembly software: {assembler}")
+    if test:
+        reads_dir=os.path.join(script_dir, "test_data", "sequences")
+        sample_info=os.path.join(script_dir, "test_data", "sample_info.txt")
+        output_dir="test_output"
+        reference_genome=os.path.join(script_dir, "db", "Fp22_genome", "fp22_assembly.fasta")
+        prophage_region=os.path.join(script_dir, "db", "Fp22_genome", "fp22_prophage_region.bed")
+
+    # write run log if it is not a dry run
+    if not dryrun:
+        os.makedirs(output_dir, exist_ok=True)
+        logfile = os.path.join(output_dir, f"{os.path.basename(output_dir)}_run.log")
+        with open(logfile, "w") as log:
+            log.write("================VTPhageFinder run log==============\n")
+            log.write(f"Start time: {datetime.now()}\n")
+            log.write(f"VTPhageFinder version: {version}\n")
+            log.write(f"Raw reads direcotry: {reads_dir}\n")
+            log.write(f"Sample table: {sample_info}\n")
+            log.write(f"Results directory: {output_dir}\n")
+            log.write(f"Host genome: {reference_genome}\n")
+            log.write(f"Host prophage coordinates: {prophage_region}\n")
+            log.write(f"Host prophage region identity cutoff: {prophage_identity}\n")
+            log.write(f"Host non-prophage region identity cutoff: {nonprophage_identity}\n")
+            log.write(f"Mapping software: {mapper}\n")
+            log.write(f"Assembly software: {assembler}")
           
-          cmd = (
-            'snakemake --snakefile workflow/Snakefile '
-                '--use-conda --conda-frontend mamba '
-                '{conda_envs} '
-                '--profile {profile} --rerun-incomplete ' 
-                '--printshellcmds --nolock --show-failed-logs '
-                '{dryrun} '
-                '--config reads_dir={reads} sample_info={meta} '
-                    'results_dir={results} host_genome={host} '
-                    'host_prophage_region={region} prophage_identity={propid} '
-                    'nonprophage_identity={nonpropid} '
-                    'mapper={mapper} assembler={assembler} step={step}'
-          ).format(
-            conda_envs='' if conda_envs=='' else '--conda-prefix {}'.format(conda_envs),
+    cmd = (
+        'snakemake --snakefile {snakefile} '
+        '--use-conda --conda-frontend mamba '
+        '--conda-prefix {envs} '
+        '--profile {profile} --rerun-incomplete ' 
+        '--printshellcmds --nolock --show-failed-logs '
+        '{dryrun} '
+        '--config reads_dir={reads} sample_info={meta} '
+        'results_dir={results} host_genome={host} '
+        'host_prophage_region={region} prophage_identity={propid} '
+        'nonprophage_identity={nonpropid} adapter={adapter} '
+        'mapper={mapper} assembler={assembler} step={step}'
+        ).format(
+            snakefile=snakefile,
+            envs=default_envs if conda_envs=='' else conda_envs,
             profile=profile,
             dryrun='--dryrun' if dryrun else '',
             reads=reads_dir,
@@ -147,17 +183,18 @@ def run_vtphagefinder(reads_dir, sample_info, output_dir, reference_genome,
             region=prophage_region,
             propid=prophage_identity,
             nonpropid=nonprophage_identity,
+            adapter=default_adapters if adapter=='' else adapter,
             mapper=mapper,
             assembler=assembler,
             step=step
-          )
+            )
 
-          # run snakemake with command-line config
-          try:
-            subprocess.run(cmd, check=True, shell=True)
-          except subprocess.CalledProcessError:
-            print("Snakemake failed. see log for details.", file=sys.stderr)
-            sys.exit(1)
+    # run snakemake with command-line config
+    try:
+        subprocess.run(cmd, check=True, shell=True)
+    except subprocess.CalledProcessError:
+        print("Snakemake failed. see log for details.", file=sys.stderr)
+        sys.exit(1)
 
 if __name__ == "__main__":
   run_vtphagefinder()
